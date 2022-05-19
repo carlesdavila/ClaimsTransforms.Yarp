056aa592a2adf2c39c1df02d0384a01ed9e4657d
carlesdavila <carlesdavila@users.noreply.github.com>
2022-05-19 23:50:24 +0200

    First commit

---
+++ build/Program.cs
@@ -0,0 +1,53 @@
+﻿using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+const string artifactsDir = "artifacts";
+
+Target(Targets.RestoreTools, async () =>
+{
+    await RunAsync("dotnet", "tool restore");
+});
+
+Target(Targets.CleanArtifactsOutput, () =>
+{
+    if (Directory.Exists(artifactsDir)) Directory.Delete(artifactsDir, true);
+});
+
+Target(Targets.CleanBuildOutput, async () =>
+{
+    await RunAsync("dotnet", "clean -c Release -v m --nologo");
+});
+
+Target(Targets.Build, DependsOn(Targets.CleanBuildOutput), async () =>
+{
+    await RunAsync("dotnet", "build -c Release --nologo");
+});
+
+Target(Targets.RunTests, DependsOn(Targets.Build), async () =>
+{
+    await RunAsync("dotnet", "test -c Release --no-build --nologo");
+});
+
+Target(Targets.GenerateArtifacts, DependsOn(Targets.CleanArtifactsOutput), async () =>
+{
+    await RunAsync("dotnet", $"pack -c Release -o {Directory.CreateDirectory(artifactsDir).FullName} --no-build --nologo");
+});
+
+Target(Targets.PublishArtifacts, DependsOn(Targets.GenerateArtifacts), () => Console.WriteLine("publish artifacts"));
+
+Target("default", DependsOn(Targets.RunTests, Targets.PublishArtifacts));
+
+await RunTargetsAndExitAsync(args);
+
+internal static class Targets
+{
+    public const string RestoreTools = "restore-tools";
+    public const string CleanBuildOutput = "clean-build-output";
+    public const string CleanArtifactsOutput = "clean-artifacts-output";
+    
+    public const string Build = "build";
+    public const string RunTests = "run-tests";
+
+    public const string GenerateArtifacts = "generate-artifacts";
+    public const string PublishArtifacts = "publish-artifacts";
+}
---
+++ src/ClaimsTransforms.Yarp/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ClaimsTransforms.Yarp;
+public class Class1
+{
+
+}

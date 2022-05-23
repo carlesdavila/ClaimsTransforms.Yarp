ea692f75343984579f798e45ead178d49ad7e832
Carlos Davila <carlesdavila@users.noreply.github.com>
2022-05-23 20:27:03 +0200

    ClaimsPrefixTransform first implementation

---
+++ src/ClaimsTransforms.Yarp/ClaimsPrefixTransform.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Yarp.ReverseProxy.Transforms;
+
+namespace ClaimsTransforms.Yarp;
+
+public class ClaimsPrefixTransform : RequestTransform
+{
+    public ClaimsPrefixTransform(PathString claimTypePattern)
+    {
+        ClaimTypePattern = claimTypePattern;
+    }
+    
+    internal PathString ClaimTypePattern { get; }
+
+    public override ValueTask ApplyAsync(RequestTransformContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        var resultPrefix = ClaimTypePattern.Value;
+        var regex = new Regex(@"(?<=\{)(.*?)(?=\})");
+        var matchGroups = regex.Matches(ClaimTypePattern.Value!);
+
+        foreach (Match matchGroup in matchGroups)
+        {
+            var claimType = matchGroup.Value;
+            var claimValue = context.HttpContext.User.Claims.FirstOrDefault(c => c.Type == claimType)?.Value;
+
+            if (claimValue != null)
+            {
+                resultPrefix = resultPrefix!.Replace($"{{{claimType}}}", claimValue);
+            }
+        }
+        var prefixPath = new PathString(resultPrefix);
+        context.Path = prefixPath + context.Path;
+
+        return default;
+    }
+}
---
+++ test/ClaimsTransforms.Yarp.Tests/ClaimsPrefixTransformTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Yarp.ReverseProxy.Transforms;
+
+namespace ClaimsTransforms.Yarp.Tests;
+
+public class ClaimsPrefixTransformTests
+{
+    [Fact]
+    public async Task ClaimsPrefixTransform_Success()
+    {
+        // Arrange
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new (ClaimTypes.Name, "someName"),
+            new (ClaimTypes.NameIdentifier, "1"),
+            new("some-claim-type", "claimValue")
+        }, "mock"));
+        
+        var context = new RequestTransformContext()
+        {
+            HttpContext = new DefaultHttpContext() { User = user },
+            Path = "/somePath"
+        };
+        
+        var transform = new ClaimsPrefixTransform($"/{{{ClaimTypes.Name}}}/anotherpath/{{some-claim-type}}");
+        
+        // Act
+        await transform.ApplyAsync(context);
+        
+        // Assert
+        context.Path.ToString().Should().Be("/someName/anotherpath/claimValue/somePath");
+    }
+}
